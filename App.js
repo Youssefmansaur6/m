9d41a55bfb157aa3b30bb99f8a11acb6fce68c7b
Youssefmansaur6 <yousefmansaur@gmail.com>
2025-03-27 22:23:55 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Bavber from './navBar'
+import Footer from './footer';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+     <Bavber/>
+     <h1>manes</h1>
+     <p>front end</p>
+    <Footer/>
+
+   
+
+      
+    </div>
+  );
+}
+
+
+export default App;
